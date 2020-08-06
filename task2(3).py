80eee3cb23165c80c8d3460e7b3525ab58a46d8a
Christi_Isaac <68722704+isaacpycad@users.noreply.github.com>
2020-08-06 12:19:06 -0400

    Create task2(3).py

---
+++ task2(3).py
@@ -0,0 +1,23 @@
+#Write a program in Python to implement the given flowchart:
+
+a = 10
+b = 20
+c = 30
+
+avg = (a + b + c) / 3
+print("avg =", avg)
+
+if avg > a and avg > b and avg > c:
+    print("%d is higher than %d, %d, %d" %(avg, a, b, c))
+elif avg > a and avg > b:
+    print("%d is higher than %d, %d" %(avg, a, b))
+elif avg > a and avg > c:
+    print("%d is higher than %d, %d" %(avg, a, c))
+elif avg > b and avg > c:
+    print("%d is higher than %d, %d" %(avg, b, c))
+elif avg > a:
+    print("%d is just higher than %d" %(avg, a))
+elif avg > b:
+    print("%d is just higher than %d" %(avg, b))
+elif avg > c:
+    print("%d is just higher than %d" %(avg, c)
